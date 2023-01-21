ce45fc2804acd95b1d241aaffca834bd445f9442
Natalia-Havashi <natali.gavashi@gmail.com>
2023-01-21 23:18:41 +0200

    moda-read-more.js

--- src/js/modal-read-more.js
+++ src/js/modal-read-more.js
@@ -1,8 +1,8 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
+      openModalBtn: document.querySelector("[data-modal-open-read]"),
+      closeModalBtn: document.querySelector("[data-modal-close-read]"),
+      modal: document.querySelector("[data-modal-read]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
