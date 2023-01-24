8e0729092d546270b67e276c39f1f77a739cda24
aggenov <a.g.genov@gmail.com>
2023-01-24 17:40:52 +0200

    поправил баг с видео и размером меню в таблете

--- src/js/modal-read-more.js
+++ src/js/modal-read-more.js
@@ -1,14 +1,33 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open-read]"),
-      closeModalBtn: document.querySelector("[data-modal-close-read]"),
-      modal: document.querySelector("[data-modal-read]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden-read");
+  const openModalBtnRead = document.querySelector("[data-modal-open-read]"),
+    closeModalBtnRead = document.querySelector("[data-modal-close-read]"),
+    modalRead = document.querySelector("[data-modal-read]"),
+    iframeRead = modalRead.querySelector('iframe');
+  openModalBtnRead.addEventListener("click", toggleModal);
+  closeModalBtnRead.addEventListener("click", toggleModal);
+  function toggleModal() {
+    modalRead.classList.toggle("is-hidden-read");
+    if (iframeRead) {
+      var iframeReadSrc = iframeRead.src;
+      iframeRead.src = iframeReadSrc;
     }
-  })();
+  }
+})();
+
+
+
+//       старая версия
+// (() => {
+//     const refs = {
+//       openModalBtn: document.querySelector("[data-modal-open-read]"),
+//       closeModalBtn: document.querySelector("[data-modal-close-read]"),
+//       modal: document.querySelector("[data-modal-read]"),
+//     };
+  
+//     refs.openModalBtn.addEventListener("click", toggleModal);
+//     refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+//     function toggleModal() {
+//       refs.modal.classList.toggle("is-hidden-read");
+//     }
+//   })();
